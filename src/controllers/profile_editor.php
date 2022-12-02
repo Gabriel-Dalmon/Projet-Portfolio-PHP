556eb2612f996ca5f5a094fb6ed4f5b541249818
Gabriel Dalmon - Home <dalmgabi@gmail.com>
2022-12-02 05:11:24 +0100

    Profile Editor ready

--- src/controllers/profile_editor.php
+++ src/controllers/profile_editor.php
@@ -1,7 +1,12 @@
 <?php
+require("/src/model/queries/getUserData.php"); 
+if(($_SESSION['user']['account_status'] == 2) AND (isset($_POST['user_id']))){
+    $user = getAllData($_POST['user_id']);
+} else {
+    $user = getAllData($_SESSION['user']['id']);
+}
 
-require("/src/model/queries/getUserData.php");
-$user = getAllData($_SESSION['user']['id']);
+
 require("/templates/patterns/profile_editor.php");
 
 
